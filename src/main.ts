e0e498bceaa768a1ef02ac67aa85d760a729600d
Biswojit Biswal <biswojitb474@gmail.com>
2026-08-17 02:35:47 +0530

    prepare app for deployment

--- src/main.ts
+++ src/main.ts
@@ -34,7 +34,7 @@ async function bootstrap() {
     new ResponseInterceptor(),
   );
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
   console.log(`Application starts on ${process.env.PORT ?? 3000}`);
 }
 bootstrap();
